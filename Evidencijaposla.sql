130a1a70ca923d2cbaea7569ae113924adcd7b47
Matej031 <matejzovak031@gmail.com>
2021-12-01 11:45:46 +0100

    Zavrsni rad - modifikacije

--- Evidencijaposla.sql
+++ Evidencijaposla.sql
@@ -2,54 +2,59 @@ drop database if exists Evidencijaposla;
 create database Evidencijaposla character set utf8;
 use Evidencijaposla;
 
-create table radnik(
+create table radnici(
     sifra int not null primary key auto_increment,
     ime varchar (50) not null,
     prezime varchar (50) not null,
     email varchar (50) not null,
-    klijent int
+    klijenti int,
+    rokovi int
 );
 
-create table Klijent(
+create table Klijenti(
     sifra int not null primary key auto_increment,
     naziv varchar (50) not null,
-    radnik int
+    email varchar (50) not null,
+    rokovi int
 );
 
-create table period_(
+create table rokovi(
     sifra int not null primary key auto_increment,
-    klijent int,
-    radnik int,
-    datumpocetka datetime,
-    datumzavrsetka datetime,
-    rok varchar (50) not null
+    datum_primitka datetime,
+    datum_zavrsetka datetime
 );
 
-alter table radnik add foreign key (klijent) references klijent(sifra);
+alter table radnici add foreign key (klijenti) references klijenti(sifra);
+alter table radnici add foreign key (rokovi) references rokovi(sifra);
 
-alter table period_ add foreign key (klijent) references klijent(sifra);
-alter table period_ add foreign key (radnik) references radnik(sifra);
+alter table klijenti add foreign key (rokovi) references rokovi(sifra);
 
-insert into radnik(sifra,ime,prezime,email) values
+insert into radnici(sifra,ime,prezime,email)
+values
 (null,'Matej','Zovak','matejzovak@gmail.com'),
 (null,'Bruno','Zovak','brunozovak@gmail.com'),
 (null,'Vedran','Burek','vedranburek@gmail.com'),
-(null,'Ante','Antunovic','anteantunovic@gmail.com'),
-(null,'Juraj','Krizanic','jurajkrizanic@gmail.com');
+(null,'Ante','Antunović','anteantunovic@gmail.com'),
+(null,'Juraj','Križanić','jurajkrizanic@gmail.com');
 
-insert into klijent(sifra,naziv) values
-(null,'nike'),
-(null,'adidas'),
-(null,'puma'),
-(null,'reebok'),
-(null,'Ufc');
+insert into klijenti(sifra,naziv,email)
+values
+(null,'nike','nike@gmail.com'),
+(null,'adidas','adidas@gmail.com'),
+(null,'puma','puma@gmail.com'),
+(null,'reebok','reebok@gmail.com'),
+(null,'Ufc','Ufc@gmail.com');
 
-insert into period_(sifra,datumpocetka,datumzavrsetka,rok) values
-(null,'2021-01-01','2021-03-31','10'),
-(null,'2021-01-01','2021-03-31','10'),
-(null,'2021-01-01','2021-03-31','10'),
-(null,'2021-01-01','2021-03-31','10'),
-(null,'2021-01-01','2021-03-31','10');
+insert into rokovi(sifra,datum_primitka,datum_zavrsetka)
+values
+(null,'2021-01-01','2021-03-31'),
+(null,'2021-01-01','2021-03-31'),
+(null,'2021-01-01','2021-03-31'),
+(null,'2021-01-01','2021-03-31'),
+(null,'2021-01-01','2021-03-31');
+
+
+
 
 
 
